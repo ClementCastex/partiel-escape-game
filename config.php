afa0a99c170d5db18225f1b96bef9b43342c262b
Clement Castex <ccastex@normandiewebschool.fr>
2025-01-31 09:38:38 +0100

    config db

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "escape-game";
+$username = "root";
+$password = "toor";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
